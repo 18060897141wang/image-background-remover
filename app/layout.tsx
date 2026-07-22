755d874041f51528b3f81fdb6bc9825e1948cf78
18060897141wang <18060897141wang@users.noreply.github.com>
2026-07-22 15:17:11 +0800

    Add Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -27,7 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <GoogleAnalytics gaId="G-H4JB8RP5PY" />
+      </body>
     </html>
   );
 }
